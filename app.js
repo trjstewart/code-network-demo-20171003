56fb984efc6128d81ff053c5e17571515cab7136
Matthew Miller <mnmiller1@me.com>
2017-10-03 18:47:16 +1000

    Added a change for merge conflict example

--- app.js
+++ app.js
@@ -27,8 +27,8 @@ app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
+  var err = new Error('Rip');
+  err.status = 403;
   next(err);
 });
 
